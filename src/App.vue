342bf0de42141e8da05fac081188dab2158ef78c
Alysson Daniel Nascimento <alyssondaniel@gmail.com>
2018-08-06 11:57:16 -0300

    Successfully completed the first part of our tutorial, connecting the Vue.js application to the Firebase real-time database

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,33 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div v-for="message in messages" :key="message.id">
+      <h4>{{ message.title }}</h4>
+      <p>{{ message.text }}</p>
+      <p>{{ message.timestamp }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import Firebase from 'firebase'
+// Initialize Firebase
+let config = {
+  apiKey: 'AIzaSyA6pY0Wk6LOEcKvNTZWnSjsp6avL_LpeHw',
+  authDomain: 'vue-app-97774.firebaseapp.com',
+  databaseURL: 'https://vue-app-97774.firebaseio.com',
+  projectId: 'vue-app-97774',
+  storageBucket: 'vue-app-97774.appspot.com',
+  messagingSenderId: '732395465798'
+}
+
+let app = Firebase.initializeApp(config)
+let db = app.database()
+let messagesRef = db.ref('messages')
+
 export default {
-  name: 'App'
+  name: 'app',
+  firebase: {
+    messages: messagesRef
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
